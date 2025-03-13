54a85554b5ce33e916fa5af1e798c6ec7c745406
kurygas <kuryshkin05@gmail.com>
2025-03-14 02:46:02 +0300

    Clearing repo

--- run.sh
+++
@@ -1,2 +0,0 @@
-cmake --build /home/kuryga/projects/ep_api/build --config Debug --target all -j 10 --
-/home/kuryga/projects/ep_api/build/ep_api --docroot . --http-address 127.0.0.1 --http-port 8080
--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-payload = {"tg_id": "1234", "token": "ekQnFd5HicZ2iQsM", "target": "Владимир"}
-response = requests.post("http://127.0.0.1:8080/user/update_profile", json=payload)
-print(response.status_code)
-print(response.content)
